18dfbe0b21a47b5a77529ce366d4853d1511a10b
ImAdityaa12 <adityagupta1291@gmail.com>
2024-12-03 14:47:27 +0530

    Minor UI Fixes and CSS Fixes

--- src/components/expertise-section.tsx
+++ src/components/expertise-section.tsx
@@ -3,26 +3,26 @@ import React from "react";
 
 const ExpertiseSection = () => {
   return (
-    <div className="bg-[#F7F7FA] h-[700px] w-full max-sm:px-4">
+    <div className="bg-[#F7F7FA] h-[700px] w-full max-sm:px-4 flex flex-col items-center justify-center">
       <div className="max-w-[1100px] mx-auto">
         <div className="relative w-full flex h-[300px] items-center">
-          <div className="w-full">
+          <div className="w-full mb-auto">
             <h1 className="font-bold text-4xl max-w-[600px]">
               Let&apos;s start new journey with me, I&apos;ve{" "}
               <span className="text-[#4BB543]">expertise</span> in these
               software&apos;s
             </h1>
-            <p className="text-sm mt-[27px] leading-[19px] text-[#333333] w-full tracking-wider">
+            <p className="text-sm mt-[27px] text-[#333333] w-full tracking-wider leading-[36px]">
               From creating wireframes and prototypes to high-fidelity mockups
               and animations, I blend design tools and fashion insights to bring
               my creations to life.
             </p>
           </div>
-          <div className="relative w-[500px] h-[400px]">
+          <div className="relative w-[500px] h-[400px] mb-24">
             <Image src={"/person-thinking.svg"} fill alt="expertise" />
           </div>
         </div>
-        <div className="mt-[40px] w-full grid grid-cols-3 gap-12">
+        <div className="w-full grid grid-cols-3 gap-12">
           <div className="flex flex-col gap-4">
             <Image
               src={"/figma.svg"}
