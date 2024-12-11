a38817f2ab1730f9fbf905fe16d1018f994d8dfb
ImAdityaa12 <adityagupta1291@gmail.com>
2024-12-11 20:47:22 +0530

    Added project categories

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,59 +1,111 @@
+"use client";
+import { projects } from "@/data/projects";
 import { ArrowRight } from "lucide-react";
 import Image from "next/image";
-import React from "react";
+import { useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
-const page = () => {
+const ProjectsPage = () => {
+  const searchParams = useSearchParams();
+  const [categoryProjects, setCategoryProjects] = useState<
+    {
+      image: string;
+      badge: string;
+      title: string;
+      url: string;
+      medal: boolean;
+      desc: string;
+    }[]
+  >([]);
+
+  const category = searchParams.get("category");
+  useEffect(() => {
+    if (category) {
+      setCategoryProjects(
+        projects[category as keyof typeof projects].project_list
+      );
+    } else {
+      const allProjects = Object.values(projects).reduce(
+        (acc, project) => [...acc, ...project.project_list],
+        [] as {
+          image: string;
+          badge: string;
+          title: string;
+          url: string;
+          medal: boolean;
+          desc: string;
+        }[]
+      );
+      setCategoryProjects(allProjects);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
     <div className="max-w-[1200px] mx-auto mb-[172px]">
-      <div className="flex items-center gap-[150px] mt-[112px]">
-        <div className="max-w-[650px]">
-          <h1 className="text-[#4BB543] font-bold">UI-UX</h1>
-          <h1 className="mt-[10px] text-3xl font-bold">05 Projects</h1>
-          <p className="mt-6">
-            Exploring the Interaction of Design and Innovation: Here are a few
-            selected projects that highlight my passion for designing intuitive
-            and impactful user experiences.
-          </p>
-          <button className=" border border-[#D5D5D5] py-2 px-5 rounded-md font-bold hover:bg-[#0A0A0A] hover:text-white transition-all mt-6">
-            View All Works
-          </button>
+      {category && (
+        <div className="flex items-center gap-[150px] mt-[112px]">
+          <div className="max-w-[650px]">
+            <h1 className="text-[#4BB543] font-bold">
+              {category ? category.toUpperCase().replace(/_/g, "-") : ""}
+            </h1>
+            <h1 className="mt-[10px] text-3xl font-bold">
+              {categoryProjects.length} Projects
+            </h1>
+            <p className="mt-6">
+              {projects[category as keyof typeof projects].heading}
+            </p>
+            <button className=" border border-[#D5D5D5] py-2 px-5 rounded-md font-bold hover:bg-[#0A0A0A] hover:text-white transition-all mt-6">
+              View All Works
+            </button>
+          </div>
+          <div className="relative w-[400px] h-[280px]">
+            <Image src={"/ui-ux-project.svg"} alt="ui-ux-project" fill />
+          </div>
         </div>
-        <div className="relative w-[400px] h-[280px]">
-          <Image src={"/ui-ux-project.svg"} alt="ui-ux-project" fill />
-        </div>
-      </div>
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
-      <ProjectCard />
+      )}
+      {categoryProjects.length > 0 &&
+        categoryProjects.map((project) => (
+          <ProjectCard {...project} key={project.title} />
+        ))}
     </div>
   );
 };
-
-function ProjectCard() {
+function ProjectCard({
+  image,
+  badge,
+  title,
+  url,
+  medal,
+  desc,
+}: {
+  image?: string;
+  badge?: string;
+  title?: string;
+  url?: string;
+  medal?: boolean;
+  desc?: string;
+}) {
   return (
     <div className="mt-[100px]">
       <div className="w-full flex gap-[55px]">
         <div className="relative min-w-[700px] h-[531px]">
-          <Image src={"/bridging-the-gap.svg"} fill alt="bridging-the-gap" />
+          <Image src={image || ""} fill alt="bridging-the-gap" />
         </div>
-        <div className="w-full h-[531px] flex flex-col gap-4">
+        <div className="w-full h-[531px] flex flex-col gap-4 pb-5">
           <div className="flex items-center gap-4 mt-auto">
-            <Image src={"/medal.svg"} width={20} height={20} alt="medal" />
+            {medal && (
+              <Image src={"/medal.svg"} width={20} height={20} alt="medal" />
+            )}
             <p className="text-xs font-bold bg-[#F7F7FA] px-3 py-2 rounded-full">
-              UI-UX Hackathon Project
+              {badge}
             </p>
           </div>
-          <h1 className="text-2xl font-bold">Bridging the Gap</h1>
-          <p className="text-sm text-[#333333] leading-[31px]">
-            Secured 3rd place nationally at the NIFT x ONDC 5-Day UI/UX Design
-            Hackathon. Our project delved into the psychographics of
-            non-shoppers, crafting innovative solutions for real-world
-            challenges in complex systems. Through in-depth research and a
-            holistic approach, we designed and optimized user-centric
-            experiences that seamlessly integrated functionality and innovation.
-          </p>
-          <button className="flex items-center text-sm font-bold text-[#4BB543]">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <p className="text-sm text-[#333333] leading-[31px]">{desc}</p>
+          <button
+            className="flex items-center text-sm font-bold text-[#4BB543]"
+            onClick={() => window.open(url, "_blank")}
+          >
             View Case Study
             <ArrowRight strokeWidth={2} />
           </button>
@@ -62,4 +114,4 @@ function ProjectCard() {
     </div>
   );
 }
-export default page;
+export default ProjectsPage;
---
+++ src/data/projects.ts
@@ -0,0 +1,124 @@
+export const projects = {
+  ui_ux: {
+    heading:
+      "Exploring the Interaction of Design and Innovation: Here are a few selected projects that highlight my passion for designing intuitive and impactful user experiences.",
+    project_list: [
+      {
+        image: "/bridging-the-gap.svg",
+        badge: "UI-UX Hackathon Project",
+        title: "Bridging the Gap",
+        url: "#",
+        medal: true,
+        desc: "Secured 3rd place nationally at the NIFT x ONDC 5-Day UI/UX Design Hackathon. Our project delved into the psychographics of non-shoppers, crafting innovative solutions for real-world challenges in complex systems. Through in-depth research and a holistic approach, we designed and optimized user-centric experiences that seamlessly integrated functionality and innovation.",
+      },
+      {
+        image: "/ar-navigator.svg",
+        badge: "Academic Project",
+        title: "AR Navigator",
+        url: "#",
+        medal: false,
+        desc: "Enhancing airport experiences with AR and VR: The Airport Guide App integrates real-time navigation, information, and entertainment to transform the way visitors interact with airports. By blending cutting-edge augmented and virtual reality, the app simplifies navigation while delivering engaging, informative content for a seamless travel experience.",
+      },
+      {
+        image: "/puspanjali.svg",
+        badge: "Academic Project",
+        title: "PUSHPANJALI",
+        desc: "This project celebrates India's vibrant flower market, highlighting the cultural and daily significance of blooms like roses, marigolds, and chrysanthemums. Research in Patna revealed urban consumers needs, inspiring an initiative to introduce local flowers to urban markets and support the growing Indian floral industry.",
+        url: "#",
+        medal: false,
+      },
+      {
+        image: "/zephyr.svg",
+        badge: "UI-UX Hackathon Project",
+        title: "ZEPHYR",
+        desc: "This project was created during the UI/UX Hackathon at IIT Patna on February, 2024, where we secured the top position. The challenge was to design a website for the clothing brand ZEPHYR, focusing on enhancing the online shopping experience. We developed a virtual reality website with a 360° product view, enabling users to make more informed and engaging purchasing decisions. By integrating immersive VR technology, ZEPHYR aims to elevate its value proposition, boosting customer satisfaction, loyalty, and sales through tailored and interactive experiences.",
+        medal: true,
+        url: "#",
+      },
+      {
+        image: "/we-care.svg",
+        badge: "UI-UX Hackathon Project",
+        title: "We Care",
+        url: "#",
+        medal: true,
+        desc: "Developed for IIIT Bengaluru's UI/UX Hackathon (Nov 2023), WE CARE secured 1st place in just 48 hours. The app addresses the critical need for a supportive online community for individuals facing mental health challenges. Using AI, WE CARE provides a safe space for users to share experiences, seek guidance, and access valuable resources, enhancing engagement, trust, and loyalty in mental health care.",
+      },
+    ],
+  },
+  fashion: {
+    heading:
+      "Fashion Meets Aesthetics and Sustainability: A Curated Collection of Projects Reflecting My Passion for Designing Thoughtful, Impactful, and Eco-Conscious Fashion Experiences.",
+    project_list: [
+      {
+        image: "/fashion1.svg",
+        badge: "Academic Project",
+        title: "SANJH - SAVERA",
+        url: "#",
+        medal: false,
+        desc: "Sanjh-Savera is a Spring-Summer 2025 streetwear collection inspired by Bihar's folk traditions and the narrative of Bidesia. Blending themes of separation and preservation, it reimagines cultural heritage through modern design, celebrating the balance between tradition and contemporary style.",
+      },
+      {
+        image: "/fashion2.svg",
+        badge: "Client Based Project",
+        title: "RUNXTRM",
+        url: "#",
+        medal: false,
+        desc: "This project focuses on creating performance-driven apparel for runners. My goal is to tackle their unique challenges, optimize performance, and elevate the running experience through thoughtful and innovative clothing design.",
+      },
+      {
+        image: "/fashion3.svg",
+        badge: "Academic Project",
+        title: "Threads of Transition",
+        url: "#",
+        medal: false,
+        desc: "This project included a thorough brand analysis to define a brand's essence. Taneira, a brand known for celebrating the elegance of Indian textiles, to style and photograph a campaign that highlights the brand's diverse aesthetic. The goal was to create a visually engaging product marketing campaign while staying true to Taneira's ethos, which balances traditional craftsmanship with modern sensibilities.",
+      },
+      {
+        image: "/fashion4.svg",
+        badge: "Academic Project",
+        title: "GARBELLA",
+        url: "#",
+        medal: false,
+        desc: `"Garbella" is a comprehensive fashion forecasting project that predicts key trends for the Spring/Summer 2025 season. This project explores the convergence of bold, contemporary styles with timeless elegance, focusing on how global influences, sustainability, and innovative materials will shape the upcoming fashion landscape. The forecast highlights colors, patterns, fabrics, silhouettes, and accessories that will define the season.`,
+      },
+      {
+        image: "/fashion5.svg",
+        badge: "Academic Project",
+        title: "SRIJAN",
+        url: "#",
+        medal: false,
+        desc: "This project is an extension of the Craft Research Development (CRD) study on Sujani, conducted as part of the curriculum in a Bihar craft cluster. It expands on the initial research and includes the curation and development of a range plan. The project was carried out by a group of six students from NIFT Patna.",
+      },
+    ],
+  },
+  research_project: {
+    heading:
+      "Collection of document, including research papers and craft documentation. Each project reflects my journey of exploring ideas, culture, and design through detailed research and storytelling.",
+    project_list: [
+      {
+        image: "/research1.svg",
+        badge: "Academic Project",
+        title: "KAHANI DHAGO KE SANG",
+        url: "#",
+        medal: false,
+        desc: "This project documents Sujni, a traditional craft of Bihar, created under the Ministry of Textiles initiative. Our journey to Alawalpur allowed us to experience the intricate art of Sujni embroidery, which tells stories through its threads. The documentation highlights the craft's process, materials, and the artisans preserving this heritage, offering a visual and narrative exploration of the community. Our goal is to promote Sujni as a vital part of India's textile legacy, bridging tradition and the future.",
+      },
+      {
+        image: "/research2.svg",
+        badge: "Academic Project",
+        title: "INTERNSHIP DOCUMENT",
+        url: "#",
+        medal: false,
+        desc: "This project showcasing my recent internship work, where I delved into both fashion and UI/UX design. In fashion, I contributed to trend research, design assistance, and prototype development. In UI/UX, I focused on user research, wireframing, and creating interactive prototypes. This collection includes key documents and design samples that reflect my hands-on experience and growth throughout the internship. ",
+      },
+      {
+        image: "/research3.svg",
+        badge: "ICORD Paper",
+        title: "A Step towards Spiritual Faith",
+        url: "#",
+        medal: false,
+        desc: "Abstract of research paper in order to publish in ICORD 2025 which talks about India's cultural significance and  flower market thrives on cultural, spiritual, and daily needs, especially in urban areas with limited green spaces. A study in Patna highlights the importance of flowers in rituals and festivals, suggesting market growth by including native varieties to meet urban demands sustainably.",
+      },
+    ],
+  },
+};
